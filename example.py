6818334e786fc473a82a7b10a94eee56fd448624
Roger Esteve <rogeresteve30@gmail.com>
2022-11-12 02:18:34 +0100

    updated example

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from gcalmanager import GCalManager
 
-calendar_id = "c_6qimrdfv0qrflqq864unsolkis@group.calendar.google.com"
+calendar_id = "c_6qimrdfv0qrf@group.calendar.google.com" # not an actual calendar id just an example
 
 gcal = GCalManager(calendar_id)
-
+gcal.create_event("Test event name", "Test description", "12:00", "17:00", "2022-12-01")
